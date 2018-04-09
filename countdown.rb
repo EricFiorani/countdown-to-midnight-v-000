47f93be37b6879d3c639ec532298f2be1d07ab4b
Eric Fiorani <efiorani305@gmail.com>
2018-04-09 15:30:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,12 @@
 #write your code here
-number = 10
 
-def countdown
+
+def countdown(number)
+  number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    return "HAPPY NEW YEAR!"
+  end
+  return "HAPPY NEW YEAR!"
 end
+
